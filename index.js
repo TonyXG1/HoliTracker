aa072b7a882a040fe4695eaa29add6ab90e75dec
Anton Stoyanov <antonz29@gmail.com>
2025-01-24 11:56:32 +0200

    Created responsiblity for smaller devices

--- index.js
+++ index.js
@@ -25,28 +25,21 @@ translate.key = process.env.GOOGLE_API_KEY;
 app.get("/", async (req, res) => {
     //Gets the current date
     const date = new Date().toISOString().split("T")[0];
-    console.log(date);
-
     try {
         //STATIC for Bulgaria 2024
         const response = await axios.get(API_URL + `/PublicHolidays?countryIsoCode=BG&validFrom=${date}&validTo=${date}`); 
         const data = response.data;
 
         // check if todays date is a holiday
-        if(data.find(item => item.startDate === date) === undefined) {
-            res.render("index.ejs", {
-                holidayEn: "Today is not a holiday",
-                holidayCountry: await translate("Today is not a holiday", { from: "en", to: "bg" })
-            });
-        } else {
-            res.render("index.ejs", {
-                holidayEn: data[0].name.find(item => item.language === "EN").text,
-                holidayCountry: data[0].name.find(item => item.language === "BG").text
-            });
-        };
+        let notHoliday = data.find(item => item.startDate === date) === undefined;
+        res.render("index.ejs", {
+            date: date,
+            holidayEn: notHoliday ? "Today is not a holiday" : data[0].name.find(item => item.language === "EN").text,
+            holidayCountry: notHoliday ? await translate("Today is not a holiday", { from: "en", to: "bg" }) : data[0].name.find(item => item.language === "BG").text
+        });
 
     } catch (error) {
-        console.log(error);
+        errorHandler();
     };
 }); 
 
@@ -58,17 +51,12 @@ app.post("/submit", async (req, res, next) => {
         const response = await axios.get(API_URL + `/PublicHolidays?countryIsoCode=${country}&validFrom=${date}&validTo=${date}`);
         const data = response.data;
         console.log(API_URL + `/PublicHolidays?countryIsoCode=${country}&validFrom=${date}&validTo=${date}`);
-        if(data.find(item => item.startDate === date) === undefined) {
-            res.render("index.ejs", {
-                holidayEn: "Today is not a holiday",
-                holidayCountry: await translate("Today is not a holiday", { from: "en", to: country.toLowerCase() })
-            });
-        } else{
-            res.render("index.ejs", {
-                holidayEn: data[0].name.find(item => item.language === "EN").text,
-                holidayCountry: data[0].name.find(item => item.language === country).text
-            });
-        }
+        let notHoliday = data.find(item => item.startDate === date) === undefined;
+        res.render("index.ejs", {
+            date: date,
+            holidayEn: notHoliday ? "Not a holiday" : data[0].name.find(item => item.language === "EN").text,
+            holidayCountry: notHoliday ? await translate("Not a holiday", { from: "en", to: country.toLowerCase()}) : data[0].name.find(item => item.language === country).text
+        })
     } catch (error) {
         res.render("index.ejs", {
             holidayEn: "Choose country and date",
@@ -78,6 +66,20 @@ app.post("/submit", async (req, res, next) => {
     next();
 });
 
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 });
+
+function errorHandler(){
+    console.error("Error fetching holiday data: ", error.message);
+    console.error("Stack trace:", error.stack);
+
+    res.status(500).render("error.ejs", {
+        message: "An error occurred while fetching holiday data. Please try again later.",
+        error: process.env.NODE_ENV === "development" ? error: {}
+    });
+}
